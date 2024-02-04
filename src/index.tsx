d88d9ca110bb353b7b67f40a166115b77010af08
Adwin Ying <adwinying@gmail.com>
2024-02-04 19:22:45 +0900

    Init tsx

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import { html } from '@elysiajs/html'
+import { HoltLogger } from "@tlscipher/holt";
+import { Elysia } from "elysia";
+
+const app = new Elysia()
+  .use(html())
+  .use(new HoltLogger().getLogger())
+  .get("/", () => (
+    <html lang="en">
+      <head>
+        <title>Hello World</title>
+      </head>
+      <body>
+        <h1>Hello World</h1>
+      </body>
+    </html>
+  ))
+  .listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
