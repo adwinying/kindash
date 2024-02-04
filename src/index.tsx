00ee705b0a996e7c2dc5eeb57f82275b2811fd55
Adwin Ying <adwinying@gmail.com>
2024-02-04 23:34:59 +0900

    Init unocss

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,32 @@ import { html } from "@elysiajs/html";
 import { HoltLogger } from "@tlscipher/holt";
 import { Elysia } from "elysia";
 
+type BaseHTMLProps = {
+  children: JSX.Element;
+};
+const BaseHTML = ({ children }: BaseHTMLProps) => {
+  return (
+    <html lang="en">
+      <head>
+        <title>kindash</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@unocss/reset/tailwind.min.css"
+        />
+        <script src="https://cdn.jsdelivr.net/npm/@unocss/runtime" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+};
+
 const app = new Elysia()
   .use(html())
   .use(new HoltLogger().getLogger())
   .get("/", () => (
-    <html lang="en">
-      <head>
-        <title>Hello World</title>
-      </head>
-      <body>
-        <h1>Hello World</h1>
-      </body>
-    </html>
+    <BaseHTML>
+      <h1>Hello World</h1>
+    </BaseHTML>
   ))
   .listen(3000);
 
